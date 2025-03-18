cf6612dad40fc3836c8000f064d151cbdbc43c55
林穎新 <harry.lin@kli.com.tw>
2025-03-18 15:17:23 +0800

    商品功能 - 實作查詢商品頁表（分頁）

--- src/main/java/com/harry/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/harry/springbootmall/dao/impl/ProductDaoImpl.java
@@ -33,6 +33,7 @@ public class ProductDaoImpl implements ProductDao {
                 " last_modified_date FROM product WHERE 1=1";
 
         Map<String , Object> map = new HashMap<>();
+        // 查詢條件
         if(productQueryParams.getCategory() != null){
             sql += " AND category=:category";
             map.put("category",productQueryParams.getCategory().name());
@@ -41,7 +42,12 @@ public class ProductDaoImpl implements ProductDao {
             sql += " AND product_name LIKE :search";
             map.put("search", "%"+productQueryParams.getSearch()+"%");
         }
+        // 排序
         sql += " ORDER BY "+ productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+        // 分頁
+        sql+=" LIMIT :limit OFFSET :offset";
+        map.put("limit",productQueryParams.getLimit());
+        map.put("offset",productQueryParams.getOffset());
 
        List<Product> productList =  namedParameterJdbcTemplate.query(sql,map, new ProductRowMapper());
 
