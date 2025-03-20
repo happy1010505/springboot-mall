c2191d1e39de6132cfb9009003cb1c52abcf4c7d
林穎新 <harry.lin@kli.com.tw>
2025-03-20 12:03:04 +0800

    帳號功能 - 檢查註冊的email 、隱藏返回的密碼

--- src/main/java/com/harry/springbootmall/dao/UserDao.java
+++ src/main/java/com/harry/springbootmall/dao/UserDao.java
@@ -4,6 +4,7 @@ import com.harry.springbootmall.dto.UserRegisterRequest;
 import com.harry.springbootmall.model.User;
 
 public interface UserDao {
+    User getUserByEmail(String email);
     User getUserById(Integer userId);
 
     Integer createUser(UserRegisterRequest userRegisterRequest);
